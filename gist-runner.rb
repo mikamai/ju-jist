156fd88a1c160645e222ab60adc9f6120f8e2b1c
Elia Schito <elia@schito.me>
2014-06-06 17:54:19 +0200

    Survive an empty field ⚠️🌴

--- gist-runner.rb
+++ gist-runner.rb
@@ -5,5 +5,6 @@ form  = $doc.querySelector('form')
 form[:onsubmit] = -> submit {
   Native(submit).preventDefault
   user_and_gist_id = input[:value]
+  return unless user_and_gist_id =~ /^\w+\/\w+$/
   $$[:location][:href] = "/#{user_and_gist_id}/index.html"
 }
